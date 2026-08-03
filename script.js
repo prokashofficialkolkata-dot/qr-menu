054f9eae1676614231d0bf18acc89af088856526
prokashofficialkolkata-dot <prokashofficial.kolkata@gmail.com>
2026-08-03 11:43:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // Firebase
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
 
-import { 
+import {
 getFirestore,
 collection,
 addDoc
-} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
+}
+from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
 
 
+
+// Firebase Config
 
 const firebaseConfig = {
 
@@ -30,8 +34,11 @@ appId: "1:860085792035:web:9907610b51cd7b73147096"
 
 const app = initializeApp(firebaseConfig);
 
-
 const db = getFirestore(app);
+
+
+
+// Global Variables
 
 let menuData = [];
 
@@ -43,155 +50,276 @@ let selectedLanguage = "en";
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 
+
+// Popular Items
+
 let popularItems = [
+
 {
 name:"Chicken Tandoori",
 image:"images/chicken-tandoori.jpg",
 price:"RM 12.00"
 },
+
 {
 name:"Nasi Lemak Ayam Goreng",
 image:"images/nasi-lemak.jpg",
 price:"RM 10.00"
 },
+
 {
 name:"Naan Cheese Mozzarella",
 image:"images/naan-cheese.jpg",
 price:"RM 15.00"
 }
+
 ];
 
-// Page Navigation
+
+
+
+// Page History
 
 let pageHistory = ["welcome"];
 
+
+
+
+// Show Page
+
 function showPage(pageId){
 
-    document.querySelectorAll("#welcome, #menuPage, #cartPage, #checkoutPage")
-    .forEach(page=>{
-        page.style.display="none";
-    });
+document.querySelectorAll(
+"#welcome, #menuPage, #cartPage, #checkoutPage"
+)
+.forEach(page=>{
+
+page.style.display="none";
+
+});
 
 
-    document.getElementById(pageId).style.display="block";
+let page=document.getElementById(pageId);
 
 
-    let back = document.getElementById("backBtn");
-    let home = document.getElementById("homeBtn");
+if(page){
 
-
-    if(back && home){
-
-        if(pageId !== "welcome"){
-
-            back.style.display="inline-block";
-            home.style.display="inline-block";
-
-        }else{
-
-            back.style.display="none";
-            home.style.display="none";
-
-        }
-
-    }
+page.style.display="block";
 
 }
+
+
+
+let back=document.getElementById("backBtn");
+
+let home=document.getElementById("homeBtn");
+
+
+if(back && home){
+
+
+if(pageId !== "welcome"){
+
+back.style.display="inline-block";
+
+home.style.display="inline-block";
+
+}else{
+
+
+back.style.display="none";
+
+home.style.display="none";
+
+}
+
+
+}
+
+
+}
+
+
+
+
+// Back
 
 function goBack(){
 
-    if(pageHistory.length > 1){
 
-        pageHistory.pop();
+if(pageHistory.length > 1){
 
-        let previousPage = pageHistory[pageHistory.length-1];
 
-        showPage(previousPage);
+pageHistory.pop();
 
-    }
+
+let previous =
+pageHistory[pageHistory.length-1];
+
+
+showPage(previous);
+
 
 }
 
 
+}
+
+
+
+
+// Home
 
 function goHome(){
 
-    pageHistory=["welcome"];
+pageHistory=["welcome"];
 
-    showPage("welcome");
+showPage("welcome");
 
 }
 
 
+
+
+// Refresh
 
 function refreshPage(){
 
-    location.reload();
+location.reload();
 
 }
+
+
+
 
 // Language
 
 function setLanguage(lang){
 
-    selectedLanguage = lang;
+selectedLanguage = lang;
 
 }
 
 
-//Popular items 
+
+
+// Show Popular Items
+
 function showPopularItems(){
 
-let section = document.getElementById("popularSection");
-let box = document.getElementById("popularItems");
 
-if(!section || !box) return;
+let section =
+document.getElementById("popularSection");
 
-section.style.display = "block";
-box.innerHTML = "";
+
+let box =
+document.getElementById("popularItems");
+
+
+
+if(!section || !box){
+
+return;
+
+}
+
+
+
+section.style.display="block";
+
+
+box.innerHTML="";
+
+
 
 popularItems.forEach(item=>{
 
+
 box.innerHTML += `
+
+
 <div class="popular-card">
-<img src="${item.image}" alt="${item.name}">
+
+
+<img src="${item.image}">
+
+
 <b>${item.name}</b>
+
+
 <p>${item.price}</p>
+
+
+
 <button onclick="addCart('${item.name}','${item.price}')">
+
 ADD
+
 </button>
+
+
+
 </div>
+
+
 `;
+
+
 
 });
 
-}
 
+    }
 // Start Menu
 
 function startMenu(type){
 
 selectedType = type;
 
+
 pageHistory.push("menuPage");
+
 
 showPage("menuPage");
 
+
+
 document.getElementById("categoryBox").innerHTML="";
+
 document.getElementById("itemBox").innerHTML="";
+
+
+
+let popular =
+document.getElementById("popularSection");
+
+
+if(popular){
+
+popular.style.display="block";
+
+}
+
+
 
 loadCSV();
 
+
 }
+
+
 
 
 // Load CSV
 
 function loadCSV(){
 
+
 fetch("menu.csv")
 
+
 .then(res=>res.text())
+
 
 .then(data=>{
 
@@ -200,6 +328,7 @@ let rows=data.split("\n");
 
 
 menuData=[];
+
 
 
 rows.slice(1).forEach(row=>{
@@ -232,204 +361,319 @@ takeaway:col[3].trim()
 });
 
 
+
 showPopularItems();
+
 
 });
 
+
 }
 
+
+
+
+// Open Category
 
 function openCategory(){
 
-    let box = document.getElementById("categoryBox");
-    let popular = document.getElementById("popularSection");
-    let itemBox = document.getElementById("itemBox");
+
+let categoryBox =
+document.getElementById("categoryBox");
 
 
-    // যদি Category খোলা থাকে → বন্ধ করবে
-    if(box.innerHTML !== ""){
-
-        box.innerHTML = "";
-
-        // category item remove
-        itemBox.innerHTML = "";
+let itemBox =
+document.getElementById("itemBox");
 
 
-        // Popular আবার দেখাবে
-        if(popular){
-
-            popular.style.display = "block";
-
-            showPopularItems();
-
-        }
-
-        return;
-
-    }
+let popular =
+document.getElementById("popularSection");
 
 
 
-    // Category খুললে Popular hide হবে
-    if(popular){
 
-        popular.style.display = "none";
+// যদি Category খোলা থাকে
+// তাহলে বন্ধ করবে
 
-    }
-
-
-    itemBox.innerHTML="";
-
-    box.innerHTML="";
+if(categoryBox.innerHTML !== ""){
 
 
-
-    let all=document.createElement("button");
-
-    all.innerHTML="ALL CATEGORY";
-
-    all.className="category";
+categoryBox.innerHTML="";
 
 
-    all.onclick=function(){
-
-        showItems("ALL");
-
-    };
-
-
-    box.appendChild(all);
+itemBox.innerHTML="";
 
 
 
-    let cats=[...new Set(menuData.map(x=>x.category))];
+// Popular আবার দেখাবে
+
+if(popular){
+
+popular.style.display="block";
+
+showPopularItems();
+
+}
 
 
-    cats.forEach(cat=>{
 
-
-        let btn=document.createElement("button");
-
-        btn.innerHTML=cat;
-
-        btn.className="category";
-
-
-        btn.onclick=function(){
-
-            showItems(cat);
-
-        };
-
-
-        box.appendChild(btn);
-
-
-    });
+return;
 
 
 }
 
 
-//show category 
+
+
+
+// Category খুললে
+
+categoryBox.innerHTML="";
+
+itemBox.innerHTML="";
+
+
+
+// Popular hide
+
+if(popular){
+
+popular.style.display="none";
+
+}
+
+
+
+
+// All Category Button
+
+
+let all =
+document.createElement("button");
+
+
+all.className="category";
+
+
+all.innerHTML="ALL CATEGORY";
+
+
+
+all.onclick=function(){
+
+showItems("ALL");
+
+};
+
+
+
+categoryBox.appendChild(all);
+
+
+
+
+// Create Category List
+
+
+let categories =
+[...new Set(menuData.map(x=>x.category))];
+
+
+
+categories.forEach(cat=>{
+
+
+let btn =
+document.createElement("button");
+
+
+btn.className="category";
+
+
+btn.innerHTML=cat;
+
+
+
+btn.onclick=function(){
+
+
+showItems(cat);
+
+
+};
+
+
+
+categoryBox.appendChild(btn);
+
+
+
+});
+
+
+
+}
+
+
+
+
+// Show Items
 
 function showItems(category){
 
-    let popularSection = document.getElementById("popularSection");
-    let categoryBox = document.getElementById("categoryBox");
-    let itemBox = document.getElementById("itemBox");
+
+let categoryBox =
+document.getElementById("categoryBox");
 
 
-    // Category select করলে Popular hide হবে
-    if(popularSection){
-        popularSection.style.display="none";
-    }
+let itemBox =
+document.getElementById("itemBox");
 
 
-    // Category list বন্ধ হবে
-    categoryBox.innerHTML="";
-
-
-    // আগের item clear
-    itemBox.innerHTML="";
-
-
-    let items;
-
-
-    if(category=="ALL"){
-
-        items = menuData;
-
-    }else{
-
-        items = menuData.filter(x=>x.category==category);
-
-    }
+let popular =
+document.getElementById("popularSection");
 
 
 
-    items.forEach(item=>{
 
+// Popular hide
 
-        let price;
+if(popular){
 
+popular.style.display="none";
 
-        if(selectedType=="DINE IN"){
-
-            price=item.dine;
-
-        }else{
-
-            price=item.takeaway;
-
-        }
+}
 
 
 
-        let div=document.createElement("div");
+
+// Category list close
+
+categoryBox.innerHTML="";
 
 
-        div.className="item";
+
+// Clear old items
+
+itemBox.innerHTML="";
 
 
-        div.innerHTML=`
-
-        <div>
-        <b>${item.name}</b><br>
-        ${price}
-        </div>
 
 
-        <button class="add-btn"
-        onclick="addCart('${item.name}','${price}')">
-        ADD
-        </button>
-
-        `;
+let items;
 
 
-        itemBox.appendChild(div);
+
+if(category=="ALL"){
 
 
-    });
+items=menuData;
+
+
+}else{
+
+
+items =
+menuData.filter(
+x=>x.category==category
+);
 
 
 }
 
+
+
+
+
+items.forEach(item=>{
+
+
+let price;
+
+
+
+if(selectedType=="DINE IN"){
+
+
+price=item.dine;
+
+
+}else{
+
+
+price=item.takeaway;
+
+
+}
+
+
+
+
+
+let div =
+document.createElement("div");
+
+
+
+div.className="item";
+
+
+
+div.innerHTML=`
+
+
+<div>
+
+<b>${item.name}</b>
+
+<br>
+
+${price}
+
+
+</div>
+
+
+
+<button class="add-btn"
+
+onclick="addCart('${item.name}','${price}')">
+
+ADD
+
+</button>
+
+
+
+`;
+
+
+
+itemBox.appendChild(div);
+
+
+
+});
+
+
+
+}
 
 // Add Cart
 
 function addCart(name,price){
 
 
-let found=cart.find(x=>x.name==name);
+let found =
+cart.find(x=>x.name==name);
 
 
 
 if(found){
 
+
 found.qty++;
+
 
 }else{
 
@@ -443,9 +687,17 @@ price:price,
 qty:1
 
 });
-localStorage.setItem("cart", JSON.stringify(cart));
+
 
 }
+
+
+
+localStorage.setItem(
+"cart",
+JSON.stringify(cart)
+);
+
 
 
 alert("Added to Cart");
@@ -455,20 +707,34 @@ alert("Added to Cart");
 
 
 
+
+
 // Show Cart
 
 function showCart(){
 
+
 if(pageHistory[pageHistory.length-1] !== "cartPage"){
-    pageHistory.push("cartPage");
+
+
+pageHistory.push("cartPage");
+
+
 }
+
+
 
 showPage("cartPage");
 
-let box=document.getElementById("cartItems");
+
+
+let box =
+document.getElementById("cartItems");
+
 
 
 box.innerHTML="";
+
 
 
 let total=0;
@@ -478,44 +744,71 @@ let total=0;
 cart.forEach((item,index)=>{
 
 
-let amount=parseFloat(
+
+let amount =
+parseFloat(
 item.price.replace("RM","")
-)*item.qty;
+)
+*
+item.qty;
 
 
-total+=amount;
+
+total += amount;
 
 
 
-box.innerHTML+=`
+box.innerHTML += `
+
 
 <div class="cart-item">
 
-${item.name}
+
+<b>${item.name}</b>
+
 
 <br>
+
 
 RM ${amount.toFixed(2)}
 
+
 <br>
 
-<button onclick="changeQty(${index},-1)">-</button>
+
+
+<button onclick="changeQty(${index},-1)">
+-
+</button>
+
+
 
 ${item.qty}
 
-<button onclick="changeQty(${index},1)">+</button>
+
+
+<button onclick="changeQty(${index},1)">
++
+</button>
+
 
 
 </div>
 
+
+
 `;
+
 
 
 });
 
 
-document.getElementById("total").innerHTML=
-"Total: RM "+total.toFixed(2);
+
+
+document.getElementById("total").innerHTML =
+
+"Total: RM " + total.toFixed(2);
 
 
 
@@ -523,37 +816,41 @@ document.getElementById("total").innerHTML=
 
 
 
-// Quantity
+
+// Change Quantity
 
 function changeQty(index,value){
 
 
-cart[index].qty+=value;
+cart[index].qty += value;
+
 
 
 if(cart[index].qty<=0){
 
+
 cart.splice(index,1);
+
 
 }
 
-localStorage.setItem("cart", JSON.stringify(cart));
+
+
+localStorage.setItem(
+"cart",
+JSON.stringify(cart)
+);
+
 
 
 showCart();
 
 
-}
-
-
-
-// Back Menu
-
-function backMenu(){
-
-goBack();
 
 }
+
+
+
 
 
 
@@ -561,94 +858,159 @@ goBack();
 
 function checkout(){
 
-    pageHistory.push("checkoutPage");
 
-    showPage("checkoutPage");
-
-
-    let box=document.getElementById("tableInput");
+pageHistory.push("checkoutPage");
 
 
-    if(selectedType=="DINE IN"){
+showPage("checkoutPage");
 
-        box.innerHTML=`
-        <input id="tableNumber" placeholder="Table Number">
-        `;
 
-    }else{
 
-        box.innerHTML="Table: TAKE AWAY";
+let box =
+document.getElementById("tableInput");
 
-    }
+
+
+if(selectedType=="DINE IN"){
+
+
+
+box.innerHTML = `
+
+
+<input id="tableNumber"
+placeholder="Table Number">
+
+
+`;
+
+
+
+}else{
+
+
+box.innerHTML =
+"Table: TAKE AWAY";
+
 
 }
 
 
-// Place Order
+
+}
+
+
+
+
+
+
+
+// Place Order Firebase
 
 async function placeOrder(){
 
 
-let customerName=document.getElementById("customerName").value;
 
-let phone=document.getElementById("phone").value;
+let customerName =
+document.getElementById("customerName").value;
+
+
+
+let phone =
+document.getElementById("phone").value;
+
 
 
 let table="TAKE AWAY";
 
 
+
 if(selectedType=="DINE IN"){
 
-table=document.getElementById("tableNumber").value;
+
+table =
+document.getElementById("tableNumber").value;
+
 
 }
 
 
 
-await addDoc(collection(db,"orders"),{
+
+
+await addDoc(
+collection(db,"orders"),
+{
 
 
 type:selectedType,
 
-tableNumber: table,
+
+tableNumber:table,
+
 
 customerName:customerName,
 
+
 phone:phone,
+
 
 items:cart,
 
+
 time:new Date(),
 
+
 status:"NEW"
+
 
 
 });
 
 
 
+
 alert("Order Sent Successfully");
+
 
 
 cart=[];
 
+
 localStorage.removeItem("cart");
+
 
 
 }
 
 
-window.setLanguage = setLanguage;
-window.startMenu = startMenu;
-window.openCategory = openCategory;
-window.showItems = showItems;
-window.addCart = addCart;
-window.showCart = showCart;
-window.checkout = checkout;
-window.backMenu = backMenu;
-window.placeOrder = placeOrder;
-window.changeQty = changeQty;
-window.goBack = goBack;
-window.goHome = goHome;
-window.refreshPage = refreshPage;
-    
+
+
+
+
+// Export Functions
+
+
+window.setLanguage=setLanguage;
+
+window.startMenu=startMenu;
+
+window.openCategory=openCategory;
+
+window.showItems=showItems;
+
+window.addCart=addCart;
+
+window.showCart=showCart;
+
+window.changeQty=changeQty;
+
+window.checkout=checkout;
+
+window.placeOrder=placeOrder;
+
+window.goBack=goBack;
+
+window.goHome=goHome;
+
+window.refreshPage=refreshPage;
