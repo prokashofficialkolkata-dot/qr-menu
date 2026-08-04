ea43f07ba9cadc262289b0627d07ebdb98b0b6fe
prokashofficialkolkata-dot <prokashofficial.kolkata@gmail.com>
2026-08-04 10:24:12 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,35 +1,101 @@
-import { initializeApp } 
+// =====================================
+// RESTORAN HAMEED'S BISTRO
+// FIREBASE.JS FINAL
+// =====================================
+
+
+// Firebase App
+
+import { initializeApp }
+
 from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
 
-import { getFirestore } 
+
+
+// Firestore
+
+import { getFirestore }
+
 from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
 
-import { getAuth } 
+
+
+// Authentication
+
+import { getAuth }
+
 from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
 
 
+
+
+
+
+// Firebase Configuration
+
 const firebaseConfig = {
 
+
 apiKey: "AIzaSyA-b4Y_1pk5QX6dTQPyy2uruB0qBb0c6s0",
-  
+
+
 authDomain: "hameed-bistro-qr-menu.firebaseapp.com",
+
 
 projectId: "hameed-bistro-qr-menu",
 
+
 storageBucket: "hameed-bistro-qr-menu.firebasestorage.app",
+
 
 messagingSenderId: "860085792035",
 
+
 appId: "1:860085792035:web:9907610b51cd7b73147096"
+
 
 };
 
 
+
+
+
+
+
+// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
+
+
+
+
+
+// Firestore Database
+
 const db = getFirestore(app);
+
+
+
+
+
+
+// Firebase Authentication
 
 const auth = getAuth(app);
 
 
-export { app, db, auth };
+
+
+
+
+
+export {
+
+app,
+
+db,
+
+auth
+
+};
