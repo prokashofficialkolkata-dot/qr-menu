00bd703e33ca827f958657a8b52b3b20711d1984
prokashofficialkolkata-dot <prokashofficial.kolkata@gmail.com>
2026-08-04 08:34:41 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,33 +1,72 @@
-// Firebase Config
+// =====================================
+// RESTORAN HAMEED'S BISTRO
+// FIREBASE CONFIG FINAL
+// =====================================
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
+import {
+initializeApp
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
 
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
+
+import {
+getFirestore
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
+
+
+import {
+getAuth
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
+
+
+
+
 
 const firebaseConfig = {
 
-apiKey: "AIzaSyA-bY4_1pk5QX6dTQPyy2uruB0qBb0c6s0",
+apiKey: "AIzaSyA-b_4Y1pk5QX6dTQPyy2uruB0qBb0c6s0",
 
-authDomain: "hameed-bistro-qr-menu.firebaseapp.com",
+authDomain:
+"hameed-bistro-qr-menu.firebaseapp.com",
 
-projectId: "hameed-bistro-qr-menu",
+projectId:
+"hameed-bistro-qr-menu",
 
-storageBucket: "hameed-bistro-qr-menu.firebasestorage.app",
+storageBucket:
+"hameed-bistro-qr-menu.firebasestorage.app",
 
-messagingSenderId: "860085792035",
+messagingSenderId:
+"860085792035",
 
-appId: "1:860085792035:web:9907610b51cd7b73147096"
+appId:
+"1:860085792035:web:9907610b51cd7b73147096"
 
 };
 
+
+
+
+
 const app = initializeApp(firebaseConfig);
+
+
 
 const db = getFirestore(app);
 
+
+
 const auth = getAuth(app);
+
+
 
 auth.languageCode = "en";
 
-export { app, db, auth };
+
+
+export {
+
+app,
+db,
+auth
+
+};
