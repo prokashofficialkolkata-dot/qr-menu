f960d8b590d8791aa186211ad085251ba3cac0cd
prokashofficialkolkata-dot <prokashofficial.kolkata@gmail.com>
2026-08-05 00:12:34 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,76 +1,39 @@
 // =====================================
 // RESTORAN HAMEED'S BISTRO
-// FIREBASE CONFIG FINAL
+// FIREBASE.JS
+// VERSION 2
 // =====================================
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
 
-import { initializeApp } 
-from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
 
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
 
-import { getFirestore } 
-from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
-
-
-import { getAuth } 
-from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
-
-
-
-
-// YOUR NEW FIREBASE PROJECT
-
-const firebaseConfig = {
-
-
-apiKey: "AIzaSyAZEby6MscIOcgp94iMqAMehOJooyo8LQk",
-
-
-authDomain: "hameeds-bistro.firebaseapp.com",
-
-
-projectId: "hameeds-bistro",
-
-
-storageBucket: "hameeds-bistro.firebasestorage.app",
-
-
-messagingSenderId: "918800284532",
-
-
-appId: "1:918800284532:web:5a927c4852f1d657c0ca21"
-
-
-};
-
-
-
-
-
-// Initialize Firebase
+ // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAZEby6MscIOcgp94iMqAMehOJooyo8LQk",
+    authDomain: "hameeds-bistro.firebaseapp.com",
+    projectId: "hameeds-bistro",
+    storageBucket: "hameeds-bistro.firebasestorage.app",
+    messagingSenderId: "918800284532",
+    appId: "1:918800284532:web:5a927c4852f1d657c0ca21"
+  };
 
 const app = initializeApp(firebaseConfig);
 
-
-
-// Firestore
+const auth = getAuth(app);
 
 const db = getFirestore(app);
 
-
-
-// Authentication
-
-const auth = getAuth(app);
-
-
-
 export {
 
-app,
-
-db,
-
-auth
+    app,
+    auth,
+    db
 
 };
