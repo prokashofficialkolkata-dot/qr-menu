7341cd8cb71fcc76f417af9d72187cd75054f605
prokashofficialkolkata-dot <prokashofficial.kolkata@gmail.com>
2026-08-04 09:26:37 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,29 +1,17 @@
-// =====================================
-// FIREBASE CONFIG
-// RESTORAN HAMEED'S BISTRO
-// =====================================
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
 
+import { getFirestore } 
+from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
 
-import { initializeApp } from 
-"https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+import { getAuth } 
+from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
 
-
-import { getFirestore } from 
-"https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
-
-
-import { getAuth } from 
-"https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
-
-
-
-
-// FIREBASE CONFIG
 
 const firebaseConfig = {
 
 apiKey: "AIzaSyA-b4Y_1pk5QX6dTQPyy2uruB0qBb0c6s0",
-
+  
 authDomain: "hameed-bistro-qr-menu.firebaseapp.com",
 
 projectId: "hameed-bistro-qr-menu",
@@ -37,27 +25,11 @@ appId: "1:860085792035:web:9907610b51cd7b73147096"
 };
 
 
-
-
-// INITIALIZE
-
 const app = initializeApp(firebaseConfig);
 
-
 const db = getFirestore(app);
-
 
 const auth = getAuth(app);
 
 
-auth.languageCode = "en";
-
-
-
-export {
-
-app,
-db,
-auth
-
-};
+export { app, db, auth };
